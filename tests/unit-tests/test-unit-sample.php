8298f609bf0000ca5a25123da58fc7bbd6b25c5b
Simon Dowdles <simon@simondowdles.com>
2016-09-08 11:53:24 +0200

    Adding mock function

--- tests/unit-tests/test-unit-sample.php
+++ tests/unit-tests/test-unit-sample.php
@@ -15,6 +15,10 @@ class UnitTests extends \PHPUnit_Framework_TestCase{
 	}
 
 	public function test_wcct_get_option(){
+	    $get_option = $this->getFunctiosMock('WCCT', 'get_option');
+        $get_option->expects($this->once())
+            ->with($this->equalTo('wcct_venue'))
+            ->willReturn('river-club');
 	    $this->assertEquals('river-club', wcct_get_option(
 	        'venue'
         ));
